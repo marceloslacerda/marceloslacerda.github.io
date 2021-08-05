9ce12eb6df3aaf7b834dc85287612506f20a9eb1
Marcelo Lacerda <marceloslacerda@gmail.com>
2021-08-05 14:14:17 -0300

    Add search function

--- lazy-task-manager/task_manager.js
+++ lazy-task-manager/task_manager.js
@@ -4,7 +4,7 @@ function getRandomInt(max) {
 
 function saveItem(key, item) {
     if(item === null) {
-        console.warn('Attemting to store null ' + key)
+        console.warn("Attemting to store null " + key)
     }
     window.localStorage.setItem(key, JSON.stringify(item))
 }
@@ -31,6 +31,13 @@ function setupUI() {
             OLD_TITLE_FIELD.innerText = task.title
             OLD_COMMENTS_FIELD.innerText = task.comments
             DONE_TASK_BUTTON.hidden = false
+            if(GET_TASK_BUTTON.myState === undefined) {
+                GET_TASK_BUTTON.myState = 1
+                GET_TASK_BUTTON.innerText = "Not that one, another task"
+            } else if (GET_TASK_BUTTON.myState == 1) {
+                GET_TASK_BUTTON.myState = 2
+                GET_TASK_BUTTON.innerText = "Another one"
+            }
         }
     })
     DONE_TASK_BUTTON.onclick=(evt=> {
@@ -40,10 +47,80 @@ function setupUI() {
         OLD_COMMENTS_FIELD.innerText = ""
         DONE_TASK_BUTTON.hidden = true
     })
+    SEARCH_FIELD.onkeyup=(evt=> {
+        const found = [], substr = evt.target.value.toLocaleLowerCase()
+        if(substr.length < 3) {
+            unsetSearchFieldset()
+            setStatus("Type at least 3 letters")
+            return
+        }
+        for(const task of manager.done) {
+            const text = (task.comments + "\n" + task.title).toLocaleLowerCase()
+            if(text.includes(substr)) {
+                found.push(task)
+            }
+        }
+        
+        if(! found.length) {
+            unsetSearchFieldset()
+            setStatus("No tasks found with the text: " + substr)
+        } else if(found.length > 1) {
+            DONE_COUNTER_WIDGET.hidden=false
+            PREV_DONE_BUTTON.hidden=false
+            NEXT_DONE_BUTTON.hidden=false
+            PREV_DONE_BUTTON.disabled=true
+            NEXT_DONE_BUTTON.disabled=false
+            setStatus(found.length + " tasks contain the text: " + substr)
+        } else {
+            DONE_COUNTER_WIDGET.hidden=true
+            PREV_DONE_BUTTON.hidden=true
+            NEXT_DONE_BUTTON.hidden=true
+            setStatus("A single task contain the text: " + substr)
+
+        }
+        if(found.length) {
+            DONE_TITLE_FIELD.innerText = found[0].title
+            DONE_COMMENTS_FIELD.innerText = found[0].comments
+            manager.foundTasks = found
+            manager.foundTasksIdx = 0
+            setDoneCounterWidget()
+        }
+    })
+    PREV_DONE_BUTTON.onclick = evt => {
+        if (manager.foundTasksIdx == -1) {
+            console.warn("Trying to go to prev with unset foundTasksIdx")
+            return
+        } else if (manager.foundTasksIdx == 0) {
+            console.warn("Trying to go to prev while at idx = 0")
+            return
+        }
+        manager.foundTasksIdx--
+        setDoneWidget()
+        if(manager.foundTasksIdx == 0) {
+            PREV_DONE_BUTTON.disabled = true
+        }
+        NEXT_DONE_BUTTON.disabled = false
+    }
+    
+    NEXT_DONE_BUTTON.onclick = evt => {
+        if (manager.foundTasksIdx == -1) {
+            console.warn("Trying to go to next with unset foundTasksIdx")
+            return
+        } else if (manager.foundTasksIdx == manager.foundTasks.length - 1) {
+            console.warn("Trying to go to next while idx is at max")
+            return
+        }
+        manager.foundTasksIdx++
+        setDoneWidget()
+        if(manager.foundTasksIdx == manager.foundTasks.length - 1) {
+            NEXT_DONE_BUTTON.disabled = true
+        }
+        PREV_DONE_BUTTON.disabled = false
+    }
 }
 
 function addTask(e) {
-    e.preventDefault();
+    e.preventDefault()
     const title = NEW_TITLE_FIELD.value
     manager.addTask(title, NEW_COMMENTS_FIELD.value)
     setStatus("Successfuly added task: " + title)
@@ -51,7 +128,29 @@ function addTask(e) {
     NEW_COMMENTS_FIELD.value = ""
     manager.saveTasks()
     NEW_TITLE_FIELD.focus()
-    return false;
+    return false
+}
+
+function unsetSearchFieldset() {
+    PREV_DONE_BUTTON.hidden=true
+    NEXT_DONE_BUTTON.hidden=true
+    DONE_COUNTER_WIDGET.hidden=true
+    DONE_TITLE_FIELD.innerText=""
+    DONE_COMMENTS_FIELD.innerText=""
+    manager.foundTasks = []
+    manager.foundTaskIdx = -1
+}
+
+function setDoneWidget() {
+    const task = manager.foundTasks[manager.foundTasksIdx]
+    DONE_TITLE_FIELD.innerText = task.title
+    DONE_COMMENTS_FIELD.innerText = task.comments
+    setDoneCounterWidget()
+}
+
+function setDoneCounterWidget() {
+    DONE_INDEX_FIELD.innerText = manager.foundTasksIdx + 1
+    DONE_TOTAL_FIELD.innerText = manager.foundTasks.length
 }
 
 function identifyDevice() {
@@ -94,7 +193,6 @@ class Task {
     constructor(title, comments) {
         this.title = title
         this.comments = comments
-        this.currentTask = null
     }
 }
 
@@ -102,6 +200,9 @@ class TaskManager {
     constructor() {
         this.waiting = []
         this.done = []
+        this.currentTask = null
+        this.foundTasks = []
+        this.foundTaskIdx = -1
     }
     addTask(title, comments) {
         this.waiting.push(new Task(title, comments))
@@ -118,11 +219,12 @@ class TaskManager {
     completeCurrentTask() {
         const idx = this.waiting.indexOf(this.currentTask)
         if(idx == -1) {
-            console.warn('Task is not in the waiting list')
+            console.warn("Task is not in the waiting list")
             return
         }
         this.waiting.splice(idx, 1)
         this.done.push(this.currentTask)
+        this.saveTasks()
     }
     getCompletedTasks() {
         return this.done
@@ -160,7 +262,7 @@ class TaskManager {
     importTasks(text) {
         const state = JSON.parse(text)
         let added = 0
-        for(const attr of ['done', 'waiting']) {
+        for(const attr of ["done", "waiting"]) {
             for(const task of state[attr]) {
                 if(state[attr].indexOf(task) == -1) {
                     state[attr].push(task)
@@ -181,12 +283,20 @@ const IMPORT_BUTTON = document.getElementById('importButton')
 const EXPORT_BUTTON = document.getElementById('exportButton')
 const GET_TASK_BUTTON = document.getElementById('getTaskButton')
 const DONE_TASK_BUTTON = document.getElementById('doneTaskButton')
+const PREV_DONE_BUTTON = document.getElementById('prevDoneButton')
+const NEXT_DONE_BUTTON = document.getElementById('nextDoneButton')
 const TASK_FORM = document.getElementById('taskForm')
 const NEW_TITLE_FIELD = document.getElementById('newTitle')
 const OLD_TITLE_FIELD = document.getElementById('oldTitle')
 const NEW_COMMENTS_FIELD = document.getElementById('newComments')
 const OLD_COMMENTS_FIELD = document.getElementById('oldComments')
+const DONE_TITLE_FIELD = document.getElementById('doneTitle')
+const DONE_COMMENTS_FIELD = document.getElementById('doneComments')
 const STATUS_FIELD = document.getElementById('status')
+const SEARCH_FIELD = document.getElementById('searchInput')
+const DONE_COUNTER_WIDGET = document.getElementById('doneCounter')
+const DONE_INDEX_FIELD = document.getElementById('doneIndex')
+const DONE_TOTAL_FIELD = document.getElementById('doneTotal')
 const STORAGE_KEY = 'tasks'
 setupUI()
 manager.restoreTasks()
